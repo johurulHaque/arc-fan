7aa64bac59fb1ea1e004595b2fc8727429dcdc75
johurulHaque <36545407+johurulHaque@users.noreply.github.com>
2018-02-17 19:33:22 +0600

    randompoint

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var x = 0,y = 3.1,a=50,b=55;
+function setup() {
+  createCanvas(710, 400);
+
+  }
+
+
+function draw() {
+background(200,100,255);
+  strokeWeight(80);
+  stroke(255,random(0,255),random(0,255));
+  //
+// fill(rgb(0,255),rgb(0,255),rgb(0,255));
+  point(200,100);
+
+
+
+}
